c7b154fbdeeb0cd03e3c4d8eee83f5c15b22cff7
MElianaOcampo <59924447+MElianaOcampo@users.noreply.github.com>
2020-01-16 15:01:57 -0500

    usuario provider alejandro

--- movilfasesoft/lib/providers/usuario_providers.dart
+++ movilfasesoft/lib/providers/usuario_providers.dart
@@ -1,27 +1,36 @@
+import 'package:flutter/material.dart';
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/usuario.dart';
+import 'package:movilfasesoft/models/usuario.dart';
 
+class UserProvider {
+  final List<ListTile> usuario1 =List();
 
-class UsuarioProviders {
+  String _url='173.16.0.100:7001';
 
-final String dominio='173.16.0.185:7001';
-final pathUsuarioFromAres='/fasesoft-web/webresources/servicios/fasafiliados/detalleUsuarioAres/';
-
-
-Future<UsuarioAres>  getUsuario(String correo) async {
-  
-   final urii= Uri.http(dominio,pathUsuarioFromAres+correo);
-   final respuesta=await http.get(urii);
-   
-   if (respuesta.statusCode!=200){
-     print('error en respuesta http');
-
-   }
-    final jsonDecodeData=json.decode(respuesta.body);
-    final user2=UsuariosFromAres.fromList(jsonDecodeData);
-
+  getusername(String user1){
+    getUser(user1).then((onValue){
+    var onValue2 = onValue;
+        return onValue2.identificacion;
+    });
     
-  return (user2.usuarios[0]);
-}
+    }
+     Future<UsuarioAres> getUser(String user1) async{
+    print('entro');
+
+    final url= Uri.http(_url,'fasesoft-web/webresources/servicios/fasafiliados/detalleUsuarioAres/'+ user1);
+    print(url);
+    final resp = await http.get(url);
+    final decodedData= json.decode(resp.body);
+   
+    UsuarioAres user= UsuarioAres.fromJson(decodedData[0]);
+    //print(user.nombre +' '+user.identificacion);
+  
+    return user;
+
+ 
+  }
+     
+  
+  }
