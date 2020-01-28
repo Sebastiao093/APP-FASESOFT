33d4d293e4cae5accbe0366ee7d83b869f79f8a9
emercado07 <59924436+emercado07@users.noreply.github.com>
2020-01-28 17:07:26 -0500

    HU_35
    lista de aportes realizados

--- movilfasesoft/lib/providers/fas_ahorro_providers.dart
+++ movilfasesoft/lib/providers/fas_ahorro_providers.dart
@@ -3,44 +3,51 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:movilfasesoft/models/ahorro.dart';
 
-
-class FasAhorroProviders{
-
+class FasAhorroProviders {
   //final String dominio='sarapdev.eastus.cloudapp.azure.com:7001';
-  final String dominio = '173.16.0.84:7001';
-  final String path='fasesoft-web/webresources/servicios/fasahorros/';
-
-
+  final String dominio = '173.16.0.25:7001';
+  final String path = 'fasesoft-web/webresources/servicios/fasahorros/';
 
   Future<Ahorros> getAhorroPermanente(String correo) async {
-
-    final String pathAhorros='aportespermanentes';
-    final uri=Uri.http(dominio,path+pathAhorros,{'correo':correo});
+    final String pathAhorros = 'aportespermanentes';
+    final uri = Uri.http(dominio, path + pathAhorros, {'correo': correo});
     print(uri);
-    final respuestaHttp =await http.get(uri);
+    final respuestaHttp = await http.get(uri);
     Ahorros ahorro;
-    if(respuestaHttp.statusCode==HttpStatus.ok){
-        final data=respuestaHttp.body;
-        List list=json.decode(data);
+    if (respuestaHttp.statusCode == HttpStatus.ok) {
+      final data = respuestaHttp.body;
+      List list = json.decode(data);
 
-        list.forEach((item){
-          ahorro= Ahorros.fromJson(item);
-        });
-      
-    }else{
-
-       print('bolsa papeto');
+      list.forEach((item) {
+        ahorro = Ahorros.fromJson(item);
+      });
+    } else {
+      print('bolsa papeto');
     }
 
     return ahorro;
-
-
   }
 
+  Future<List<Ahorros>> getMovimientosAporte(String correo) async {
+    final String pathAhorros = 'movimientosAportes';
 
+    final uri = Uri.http(dominio, path + pathAhorros, {'correo': correo});
 
+    final respuestaHttp = await http.get(uri);
 
+    List<Ahorros> mov = [];
 
+    if (respuestaHttp.statusCode == HttpStatus.ok) {
+      final data = respuestaHttp.body;
 
+      List listaMovimientos = json.decode(data);
 
+      listaMovimientos.forEach((item) {
+        mov.add(Ahorros.fromJson(item));
+      });
+    } else {
+      print('ERROR en respuesta Http');
+    }
+    return mov;
+  }
 }
--- movilfasesoft/lib/screens/logedIn.dart
+++ movilfasesoft/lib/screens/logedIn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:movilfasesoft/main.dart';
 import 'package:movilfasesoft/models/PerfilRol.dart';
 import 'package:movilfasesoft/models/ahorro.dart';
@@ -23,8 +24,9 @@ import 'package:movilfasesoft/screens/codigoQr.dart';
 import 'package:movilfasesoft/utils/numberFormat.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-void irVotaciones(BuildContext ctx,bool preguntasPorContestar) {
-  Navigator.of(ctx).pushNamed(PantallaVotaciones.routedname,arguments: preguntasPorContestar);
+void irVotaciones(BuildContext ctx, bool preguntasPorContestar) {
+  Navigator.of(ctx).pushNamed(PantallaVotaciones.routedname,
+      arguments: preguntasPorContestar);
 }
 
 void irAsambleas(BuildContext ctx) {
@@ -62,6 +64,8 @@ class Logedin extends StatelessWidget {
   final String user = MyApp.correoUsuario;
   static String tipoRol;
   static Future<PerfilRol> futurePerfilRol;
+  DateFormat dateConvert = DateFormat("yyyy-MM-dd'T'HH:mm:ss");
+  DateFormat dateFormat = DateFormat("yyyy MMMM dd");
 
   Widget build(BuildContext context) {
     futurePerfilRol = cargarPerfilRol(user);
@@ -73,18 +77,17 @@ class Logedin extends StatelessWidget {
         } else {
           this.usuarioAres = snapshot.data;
           return Scaffold(
-            appBar: AppBar(
-              title: ImageIcon(
-                AssetImage('assets/icons/fasesoftLogoBarra.png'),
-                size: 150.0,
+              appBar: AppBar(
+                title: ImageIcon(
+                  AssetImage('assets/icons/fasesoftLogoBarra.png'),
+                  size: 150.0,
+                ),
+                centerTitle: true,
               ),
-              centerTitle: true,
-            ),
-            drawer: SafeArea(
-              child: _drawer(context),
-            ),
-            body: _DetallesAhorro(user),
-          );
+              drawer: SafeArea(
+                child: _drawer(context),
+              ),
+              body: _widget());
         }
       },
     );
@@ -95,10 +98,9 @@ class Logedin extends StatelessWidget {
         child: ListView(
       children: <Widget>[
         UserAccountsDrawerHeader(
-          accountName: Text(nombre(this.usuarioAres)),
-          accountEmail: Text(user),
-          currentAccountPicture: userPhoto(MyApp.correoUsuario)
-        ),
+            accountName: Text(nombre(this.usuarioAres)),
+            accountEmail: Text(user),
+            currentAccountPicture: userPhoto(MyApp.correoUsuario)),
         ListTile(
           leading: Icon(Icons.person, color: Colors.blue),
           title: Text('Detalle de perfil'),
@@ -127,14 +129,14 @@ class Logedin extends StatelessWidget {
           onTap: () => irQr(context),
         ),
 
-ListTile(
+        ListTile(
           leading: Icon(Icons.center_focus_weak),
           title: Text(
             'Generar QR ',
             style: TextStyle(color: Colors.blueAccent),
           ),
           onTap: () {
-           Navigator.of(context).pushNamed('/qr');
+            Navigator.of(context).pushNamed('/qr');
           },
         ),
         ListTile(
@@ -152,7 +154,6 @@ ListTile(
           title: Text('Asistencia'),
           onTap: () => irQr(context),
         )
-        
       ],
     ));
   }
@@ -181,127 +182,204 @@ ListTile(
 
   Widget _Ahorros(BuildContext context, Ahorros ahorro) {
     Size size = MediaQuery.of(context).size;
-    return ListView( children: <Widget>[
-      
-      Container(
-      padding: EdgeInsets.all(30.0),
-      child: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.blue,
-            ),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)),
-              elevation: 45.0,
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-                child: Column(
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage('assets/icons/ahorroLogo.png'),
-                      //Iconos diseñados por <a href="https://www.flaticon.es/autores/itim2101" title="itim2101">itim2101</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
-                    ),
-                    Center(
-                        heightFactor: 3.0, child: Text('DETALLES DE CUENTA ')),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10.0),
-                      child: Column(
-                        children: <Widget>[
-                          Divider(),
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 10.0),
-                            child: Column(
-                              children: <Widget>[
-                                Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Text('AHORROS')),
-                                Align(
-                                    alignment: Alignment.topRight,
-                                    child: Text(
-                                      '\$ ' +
-                                          numberFormat(ahorro.monto.toDouble()),
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.bold),
-                                    )),
-                              ],
-                            ),
+    return ListView(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  elevation: 45.0,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                    child: Column(
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/icons/ahorroLogo.png'),
+                          //Iconos diseñados por <a href="https://www.flaticon.es/autores/itim2101" title="itim2101">itim2101</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
+                        ),
+                        Center(
+                            heightFactor: 3.0,
+                            child: Text('DETALLES DE CUENTA ')),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10.0),
+                          child: Column(
+                            children: <Widget>[
+                              Divider(),
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 10.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Text('AHORROS')),
+                                    Align(
+                                        alignment: Alignment.topRight,
+                                        child: Text(
+                                          '\$ ' +
+                                              numberFormat(
+                                                  ahorro.monto.toDouble()),
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                  ],
+                                ),
+                              ),
+                              Divider(),
+                              Container(
+                                margin: EdgeInsets.only(top: 10.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Text('APORTE MENSUAL')),
+                                    Align(
+                                        alignment: Alignment.topRight,
+                                        child: Text(
+                                          '\$ ' +
+                                              numberFormat(
+                                                  ahorro.aporte.toDouble()),
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                    Divider(),
+                                    GestureDetector(
+                                      child: Text(MyApp.token),
+                                      onLongPress: () {
+                                        Clipboard.setData(new ClipboardData(
+                                            text: MyApp.token));
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                          Divider(),
-                          Container(
-                            margin: EdgeInsets.only(top: 10.0),
-                            child: Column(
-                              children: <Widget>[
-                                Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Text('APORTE MENSUAL')),
-                                Align(
-                                    alignment: Alignment.topRight,
-                                    child: Text(
-                                      '\$ ' +
-                                          numberFormat(
-                                              ahorro.aporte.toDouble()),
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.bold),
-                                    )),
-                                Divider(),
-                                GestureDetector(
-                                  child: Text(MyApp.token),
-                                  onLongPress:(){
-                                    Clipboard.setData(new ClipboardData(text: MyApp.token));
-                                  },)
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Container()
+            ],
           ),
-          Container()
-        ],
-      ),
-    )
-    ],
+        )
+      ],
     );
   }
-  static Future<PerfilRol> cargarPerfilRol(String correo) async  {
+
+  static Future<PerfilRol> cargarPerfilRol(String correo) async {
     final perfilProvider = PerfilRolProvider();
     PerfilRol perfilRol = await perfilProvider.getPerfilRol(correo);
     //tipoRol = perfilRol.tipo;
     return perfilRol;
   }
 
-Widget validacionRol(BuildContext ctx) {
-  return FutureBuilder <PerfilRol>(
+  Widget validacionRol(BuildContext ctx) {
+    return FutureBuilder<PerfilRol>(
       future: futurePerfilRol,
       builder: (ctx, perfilAux) {
-        if(perfilAux.hasData){
-          if (perfilAux.data.tipo ==  'ASISTENCIA') {
+        if (perfilAux.hasData) {
+          if (perfilAux.data.tipo == 'ASISTENCIA') {
             return ListTile(
-          leading: Icon(Icons.filter_center_focus, color: Colors.blue),
-          title: Text('Asistencia'),
-          onTap: () => irQr(ctx),
-          );
+              leading: Icon(Icons.filter_center_focus, color: Colors.blue),
+              title: Text('Asistencia'),
+              onTap: () => irQr(ctx),
+            );
           } else {
+            return Container();
+          }
+        } else if (perfilAux.hasError) {
           return Container();
         }
-        } else if (perfilAux.hasError) {
-        return Container();
-      }
+      },
+    );
+  }
+
+  Widget _movimientosAportes(String correo) {
+    FasAhorroProviders provider = FasAhorroProviders();
+
+    return FutureBuilder(
+      future: provider.getMovimientosAporte(correo),
+      builder: (context, AsyncSnapshot<List<Ahorros>> snap) {
+        if (snap.hasData) {
+          return _detallesMovimientosAportes(snap.data);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Widget _detallesMovimientosAportes(List<Ahorros> movimientos) {
+    Widget list = ListView.builder(
+      itemCount: movimientos.length,
+      itemBuilder: (ctx, posicion) {
+        String tipoAporte = 'Permanente';
+        String sinFecha = 'Fecha: Indefinida';
+        if (movimientos[posicion].fasTiposAhoIdTipoAho == 2) {
+          tipoAporte = 'Voluntario';
+        }
+
+        if (movimientos[posicion].fechaInicioAporte != null) {
+          try {
+            DateTime fecha =
+                dateConvert.parse(movimientos[posicion].fechaInicioAporte);
+            sinFecha = 'Fecha: ' + dateFormat.format(fecha);
+          } on FormatException {
+            sinFecha = 'Fecha: ' + movimientos[posicion].fechaInicioAporte;
+          }
+        }
+
+        return Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(
+                  Icons.monetization_on,
+                  color: Colors.green,
+                ),
+                trailing: Text(
+                    '\$ ' +
+                        numberFormat(
+                          movimientos[posicion].aporte.toDouble(),
+                        ),
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal),
+                    overflow: TextOverflow.ellipsis),
+                title: Text(
+                  tipoAporte,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(sinFecha, overflow: TextOverflow.ellipsis),
+              ),
+            ),
+          ],
+        );
       },
     );
 
-  
+    return list;
+  }
 
-}
+  Widget _widget() {
+    return
+        // _DetallesAhorro(user);
+        _movimientosAportes(user);
+  }
 }
 
 Widget validacionVotacion(BuildContext ctx) {
@@ -310,19 +388,19 @@ Widget validacionVotacion(BuildContext ctx) {
         Votaciones_providers.getValidacionBotonVotaciones(MyApp.correoUsuario),
     builder: (ctx, validacionAux) {
       if (validacionAux.hasData) {
-        if(validacionAux.data.hayAsamblea==null ||
-            validacionAux.data.asistio==null ||
-            validacionAux.data.preguntasPorContestar==null){
-              return Container();
-
-            }
+        if (validacionAux.data.hayAsamblea == null ||
+            validacionAux.data.asistio == null ||
+            validacionAux.data.preguntasPorContestar == null) {
+          return Container();
+        }
         if (validacionAux.data.hayAsamblea &&
             validacionAux.data.asistio &&
             validacionAux.data.preguntasPorContestar) {
           return ListTile(
             leading: Icon(Icons.question_answer, color: Colors.blue),
             title: Text('Votaciones'),
-            onTap: () => irVotaciones(ctx,validacionAux.data.preguntasPorContestar),
+            onTap: () =>
+                irVotaciones(ctx, validacionAux.data.preguntasPorContestar),
           );
         } else {
           return Container();
