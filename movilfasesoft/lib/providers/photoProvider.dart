97351ef052cde9095bb4115486dbbef1579de59b
Alej6995 <59695058+Alej6995@users.noreply.github.com>
2020-01-28 14:18:47 -0500

    photo

--- movilfasesoft/lib/providers/photoProvider.dart
+++ movilfasesoft/lib/providers/photoProvider.dart
@@ -4,26 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:movilfasesoft/main.dart';
 
-Widget userPhoto(correo) {
+Widget userPhoto(context,correo) {
  
 
   String _url='Https://graph.microsoft.com/v1.0/users/';
  
- return Container(
-      width: 100.0,
-      height: 100.0,
-      decoration: new BoxDecoration(
-          shape: BoxShape.circle,
-          image: new DecorationImage(
-          fit: BoxFit.fill,
-          image:  NetworkImage(
+ return  ClipRRect(
+   borderRadius: BorderRadius.circular(80),
+   child:FadeInImage(
+     placeholder: AssetImage('assets/icons/person.png'),
+     image:  NetworkImage(
         _url+correo+'/photo/\$VALUE' ,
         headers: {HttpHeaders.authorizationHeader: MyApp.token},
+      )
     ),
+   );
+      
    
-                 )
-)
-);
-  }
 
+  }
 
