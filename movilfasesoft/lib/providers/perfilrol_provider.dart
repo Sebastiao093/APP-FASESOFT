4500a847ba7e1cddb234aa8ca7eb1246cfa9c773
Sebastian Giraldo <shgarcia@asesoftware.com>
2020-01-29 10:39:30 -0500

    Cambio a sarapdev
    cambio de dirección

--- movilfasesoft/lib/providers/perfilrol_provider.dart
+++ movilfasesoft/lib/providers/perfilrol_provider.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:movilfasesoft/models/PerfilRol.dart';
 
 class PerfilRolProvider {
-  //final String dominio='sarapdev.eastus.cloudapp.azure.com:7001';
-  final String dominio = '173.16.0.44:7001';
+  final String dominio='sarapdev.eastus.cloudapp.azure.com:7001';
+  //final String dominio = '173.16.0.44:7001';
   final String path =
       'fasesoft-web/webresources/servicios/fasusuarios/perfilPorCorreo/';
 
