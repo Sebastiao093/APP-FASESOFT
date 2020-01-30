77b7b8011dff71af4476a19bd9f99a472fb96e7d
Asalgado02 <Asalgado02@users.noreply.github.com>
2020-01-30 16:08:01 -0500

    Cambios widget asistencia

--- movilfasesoft/lib/providers/info_asistente_providers.dart
+++ movilfasesoft/lib/providers/info_asistente_providers.dart
@@ -6,34 +6,22 @@ import 'package:http/http.dart' as http;
 import 'package:movilfasesoft/models/infoAsistente.dart';
 
 class InfoAsistenteProvider {
- 
- 
   final String dominio='sarapdev.eastus.cloudapp.azure.com:7001';
   //final String dominio = '173.16.0.32:7001';
   final String path='fasesoft-web/webresources/servicios/fasasistentes/';
-  
   String fecha = DateFormat.y().format(DateTime.now());
-  
   Future<InfoAsistente> getInfoAsistente(String correo) async {
     final String pathAsistentes='asistenciaPorFechaCorreo/'+fecha+'/'+correo;
     final url = Uri.http(dominio, path+pathAsistentes);
     print('Direccion: $url');
     InfoAsistente infoasistente;
-
     final respuestaHttp = await http.get(url);
-    
     if (respuestaHttp.statusCode == 200) {
       final decodedData = json.decode(respuestaHttp.body);
       infoasistente = InfoAsistente.fromJson(decodedData[0]);
-
-      //return decodedData;
     } else {
       throw Exception('error');
     }
-
- 
-
     return infoasistente;
-
   }
 }
