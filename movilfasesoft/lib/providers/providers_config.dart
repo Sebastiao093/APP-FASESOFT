bf25fde5984ca45b864975be50f00931e5dad308
Alej6995 <59695058+Alej6995@users.noreply.github.com>
2020-02-27 08:59:17 -0500

    cambio de endpoint prod

--- movilfasesoft/lib/providers/providers_config.dart
+++ movilfasesoft/lib/providers/providers_config.dart
@@ -1,4 +1,5 @@
 class ProviderConfig {
-   static final String url='sarapdev.eastus.cloudapp.azure.com:7001';
-   static final String path='fasesoft-web/webresources/servicios/';
+  //static final String url='sarapdev.eastus.cloudapp.azure.com:7001';
+  static final String url='asesoftwareapp.eastus.cloudapp.azure.com:7001';
+  static final String path='fasesoft-web/webresources/servicios/';
 }
